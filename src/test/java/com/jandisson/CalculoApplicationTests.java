667a0abb4ea71fa90517c07a5b75bdd422266135
Jandisson <jandissontalk@gmail.com>
2016-10-24 21:56:08 -0200

    Testes Adicionados.

--- src/test/java/com/jandisson/CalculoApplicationTests.java
+++ src/test/java/com/jandisson/CalculoApplicationTests.java
@@ -5,12 +5,22 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testaSoma()
+	{
+		Calculadora calculadora = new Calculadora();
+		assertEquals(calculadora.soma(15,30),45,0);
+
 	}
 
 }
